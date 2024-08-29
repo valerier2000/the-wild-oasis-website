da3d07f2a30acc06f71fccbef2bdd8c344622c9f
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-08-29 21:20:50 +0200

    Build the Home Page

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefinSans.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+        className={`${josefinSans.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
         <Header />
 
