20421299e7cc220e86a9ba983976a98904f94f06
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-08-29 20:53:20 +0200

    Load and Optimize Fonts

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,13 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefinSans = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 import "@/app/_styles/globals.css";
 
 export const metadata = {
@@ -15,7 +22,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefinSans.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
