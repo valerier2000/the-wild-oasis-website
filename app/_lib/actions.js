636c4b211fe5258eefb7a0dfb9903338ad12b753
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-08-31 19:29:58 +0200

    Delete a Reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,10 +3,11 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
-  if (!session) throw new Error("YOu must be logged in");
+  if (!session) throw new Error("You must be logged in");
 
   const nationalID = formData.get("nationalID");
   const [nationality, countryFlag] = formData.get("nationality").split("%");
@@ -24,6 +25,26 @@ export async function updateGuest(formData) {
   if (error) throw new Error("Guest could not be updated");
 
   revalidatePath("/account");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const questBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!questBookingIds.includes(bookingId))
+    throw new Error("You are not authallowedorized to delete this booking");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted");
+
+  revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
